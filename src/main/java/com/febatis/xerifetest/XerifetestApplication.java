2ed5ce277113959acbe99bc141f3c7d5a2e8ef7c
Lucas Batista <lfebatis@gmail.com>
2020-01-19 20:46:33 -0300

    Adicionando projeto do Spring Initializr

---
+++ src/main/java/com/febatis/xerifetest/XerifetestApplication.java
@@ -0,0 +1,13 @@
+package com.febatis.xerifetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XerifetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XerifetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/febatis/xerifetest/XerifetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.febatis.xerifetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XerifetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
