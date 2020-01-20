6f348367418666ac9489f22d85e7cc955afff2b1
Lucas Batista <lfebatis@gmail.com>
2020-01-20 02:18:14 -0300

    Adicionandio update e delete em veiculo

--- src/main/java/com/febatis/xerifetest/controller/VeiculoController.java
+++ src/main/java/com/febatis/xerifetest/controller/VeiculoController.java
@@ -8,27 +8,35 @@ import com.febatis.xerifetest.service.VeiculoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping("/veiculo")
+@RequestMapping("veiculo")
 public class VeiculoController {
 
 	@Autowired
 	private VeiculoService veiculoService;
 
 	@GetMapping()
-	public List<Veiculo> veiculo() {
-		return veiculoService.findAll();
+	public ResponseEntity<List<Veiculo>> findAll() {
+		return ResponseEntity.ok(veiculoService.findAll());
+	}
+
+	@GetMapping(path = "{id}")
+	public ResponseEntity<Veiculo> findById(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(veiculoService.findById(id).get());
 	}
 
 	@PostMapping()
-	public ResponseEntity<Object> createVeiculo(@RequestBody Veiculo veiculo) {
+	public ResponseEntity<Object> create(@RequestBody Veiculo veiculo) {
 		Veiculo savedVeiculo = veiculoService.save(veiculo);
 
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -37,4 +45,26 @@ public class VeiculoController {
 		return ResponseEntity.created(location).build();
 
 	}
+
+	@PutMapping(path = "{id}")
+	public ResponseEntity<Object> update(@PathVariable("id") Long id, @RequestBody Veiculo veiculo) {
+		veiculo.setId(id);
+		Veiculo updatedVeiculo = veiculoService.update(veiculo);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(updatedVeiculo.getId()).toUri();
+
+		return ResponseEntity.ok(location);
+
+	}
+
+	@DeleteMapping(path = "{id}")
+	public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
+		
+		veiculoService.deleteById(id);
+
+		return ResponseEntity.ok().build();
+
+	}
+
 }
--- src/main/java/com/febatis/xerifetest/service/VeiculoService.java
+++ src/main/java/com/febatis/xerifetest/service/VeiculoService.java
@@ -1,6 +1,7 @@
 package com.febatis.xerifetest.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.febatis.xerifetest.entity.Veiculo;
 import com.febatis.xerifetest.repository.VeiculoRepository;
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class VeiculoService{
+public class VeiculoService {
 
     @Autowired
     private VeiculoRepository veiculoRepository;
@@ -18,8 +19,19 @@ public class VeiculoService{
         return veiculoRepository.findAll();
     }
 
+    public Optional<Veiculo> findById(Long id) {
+        return veiculoRepository.findById(id);
+    }
+
     public Veiculo save(Veiculo veiculo) {
         return veiculoRepository.save(veiculo);
     }
 
+    public Veiculo update(Veiculo veiculo) {
+        return veiculoRepository.save(veiculo);
+    }
+
+    public void deleteById(Long id) {
+        veiculoRepository.deleteById(id);
+    }
 }
